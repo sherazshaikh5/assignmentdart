cebd830fbd36970dbfd8c1ec393690a8ff75aee6
Sheraz Shaikh <98872326+sherazshaikh5@users.noreply.github.com>
2022-02-10 00:48:34 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,49 @@
+void main() {
+
+    // Task 01
+    
+print("Twinkle,  twinkle,  little  star,\n\tHow  I  Wonder  What You  are !\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\n\tTwinkle,  twinkle,  little  star,\nHow  I  Wonder  What You  are");
+
+
+// Task 02
+
+var fname ="Anus";
+var lname ="Shaikh";
+int age = 18;
+num salary = 15000;
+
+print("User Name is ${fname}");
+print("User Last Name is ${lname}");
+print("User Age is : " + age.toString());
+print("User Salary is ${salary}");
+
+
+//  Task 03
+
+print("fname Type : ${fname.runtimeType}");
+print("lname Type : ${lname.runtimeType}");
+print("Age Type : ${age.runtimeType}");
+print("Salary Type : ${salary.runtimeType}");
+
+
+
+//  Task 04
+
+num n1 = 57;
+num n2 = 8;
+
+num sum = n1 + n2;
+num sub = n1 - n2;
+num multi = n1 * n2;
+num div = n1 / n2;
+num intdiv = n1 ~/ n2;
+
+print(sum);
+print(sub);
+print(multi);
+print(div);
+print(intdiv);
+
+
+
+}
