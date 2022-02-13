62b99df1cdfd697de6aa10f5625db8ecf627a0d6
Sheraz Shaikh <98872326+sherazshaikh5@users.noreply.github.com>
2022-02-14 01:46:55 +0500

    Add files via upload

---
+++ assignment1.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+
+void main() {
+  // // Task 01
+  //   stdout.write("First Nmae :");
+  //   var firstname =stdin.readLineSync()!;
+  //   stdout.write("Last Nmae :");
+  //   var lastname =stdin.readLineSync()!;
+  //   stdout.write("Quantity Value :");
+  //   var quantity =stdin.readLineSync()!;
+  //   print("${firstname} ${lastname} ordered ${quantity} on xyz clothig store");
+
+  //   // Task 02,03
+
+  //   stdout.write("Enter first value :");
+  //   var a =int.parse(stdin.readLineSync()!);
+  //   stdout.write("Enter second value :");
+  //   var a1 =int.parse(stdin.readLineSync()!);
+  //   print("Sum :${a+a1}");
+  //   print("Sub :${a-a1}");
+  //   print("Multi :${a*a1}");
+  //   print("Div :${a/a1}");
+  //   print("intdiv :${a~/a1}");
+
+  //   // Task 04
+
+  //   stdout.write("Enter the Tickets Price :");
+  //   var a3 =int.parse(stdin.readLineSync()!);
+  //   stdout.write("Enter Number Tickets :");
+  //   var a4 =int.parse(stdin.readLineSync()!);
+  //   print("Total Tickets Price is :${a3*a4}");
+
+//   stdout.write("Enter the value Celsius :");
+// var c =int.parse(stdin.readLineSync()!);
+// var f =(c*9/5)+32;
+// print("the value is farhrenheit ${f}");
+// print("\n");
+
+// print("SHOPPING CART:\n");
+// var a=100; var b=50;
+// stdout.write("the price of book :${a}\n");
+// stdout.write("the price of beg :${b}\n");
+// stdout.write("the quantity of book :");
+// var quantity1 =int.parse(stdin.readLineSync()!);
+// stdout.write("the quantity of beg :");
+// var quantity2 =int.parse(stdin.readLineSync()!);
+// print("your ordered total cost is ${(a*quantity1+b*quantity2)}");
+// print("\n");
+
+// var total_marks =1000;
+// stdout.write("Total marks :${total_marks}\n");
+// stdout.write("Enter the obtained marks :");
+// var ot =int.parse(stdin.readLineSync()!);
+// print("your percentege is ${(ot/total_marks)*100}");
+// print("\n");
+
+// var dollar =175.34;var riyal =46.73;
+// stdout.write("Enter the number of dollar :");
+// var numdollar =int.parse(stdin.readLineSync()!);
+// stdout.write("Enter the number of riyal :");
+// var numriyal =int.parse(stdin.readLineSync()!);
+// print("Your total amount rupees of Pakistan ${(dollar*numdollar+riyal*numriyal)} ");
+// print("\n");
+
+// var hprice=200;
+// stdout.write("Per hour price is ${hprice}\nEnter number of hours :");
+// var hour=int.parse(stdin.readLineSync()!);
+// print("Your total weekly earning is ${hprice*hour}");
+
+
+}
