148203518bcf86f7ec4aaadf838ccc3a48a768cb
Sheraz Shaikh <98872326+sherazshaikh5@users.noreply.github.com>
2022-02-15 23:11:53 -0800

    Add files via upload

---
+++ practicemain.dart
@@ -0,0 +1,181 @@
+import 'dart:io';
+
+void main() {
+  // stdout.write("Enter the City :");
+  // var city = stdin.readLineSync()!;
+  // if (city == "karachi") {
+  //   print("Welcom to City of Light, ${city}");
+  // } else if (city == "Karachi") {
+  //   print("Welcom to City of Light, ${city}");
+  // } else if (city == "Hyderabad") {
+  //   print("The City of Vantilation, ${city}");
+  // } else if (city == "hyderabad") {
+  //   print("The City of Vantilation: ${city}");
+  // } else if (city == "HYDERABAD") {
+  //   print("The City of Vantilation: ${city}");
+  // } else {
+  //   print("Not Available this city");
+  // }print("\n");
+
+  // stdout.write("Enter the Gender :");
+  // String gender = (stdin.readLineSync()!.toLowerCase());
+  // if (gender == "male") {
+  //   print("Good Morning, Sir");
+  // } else if (gender == "female") {
+  //   print("Good Morning,Madam");
+  // }else{
+  //   print("not available gender");
+  // }print("\n");
+
+  // stdout.write("Enter the temperature :");
+  // int t=int.parse(stdin.readLineSync()!);
+  // if(t>40){
+  //   print("It is too boiling hot outside.");
+  // }
+  // else if(t>35 || t==40){
+  //   print("It is too hot outside.");
+  // }else if(t>25 || t==30){
+  //   print("The Weather today is Normal.");
+  // }else if(t>15 || t==20){
+  //   print("Weather is cool.");
+  // }else if(t>5 || t==10){
+  //   print("weather is so Cool.");
+  // }else if(t<0){
+  //   print("weather is too cold");
+  // }else{
+  //   print("Weather is freezing Cold");
+  // }print("\n");
+  // int l=25;
+  // int s=20;
+  // stdout.write("Enter the First value :");
+  // int larger=int.parse(stdin.readLineSync()!);
+  // stdout.write("Enter the Last value :");
+  // int small=int.parse(stdin.readLineSync()!);
+  // if(larger==small){
+  //   print("${larger} is Equal to ${small}");
+  // }
+  // else if(larger>small){
+  //   print("${larger} is larger number");
+  //   print("${small} is small number");
+  // }else if(larger<small){
+  //   print("${small} is larger number");
+  //   print("${larger} is larger number");
+  // }
+  
+
+  // stdout.write("Enter the traffic signal color :");
+  // var color=stdin.readLineSync();la
+  // if(color=="red"){
+  //   print("The signal is red, Stop");
+  // }else if(color=="Red"){
+  //   print("The signal is red, Stop");
+  // }else if(color=="yellow"){
+  //   print("The signal is yellow, Ready to move");
+  // }else if(color=="Yellow"){
+  //   print("The signal is yellow, Ready to move");
+  // }else if(color=="green"){
+  //   print("The signal is green, Move now");
+  // }else if(color=="Green"){
+  //   print("The signal is green, Move now");
+  // }else{
+  //   print(" ");
+  }
+  // else if(color=="yellow"){
+  //   print("The signal is yellow, Ready to move");
+  // }else if(color=="green"){
+  //   print("The signal is green, Move now");
+  // }
+
+  // print('Enter a number');
+  //   var line = stdin.readLineSync();
+  //   int a = int.parse(stdin.readLineSync()!);
+
+  //   if(a<0){
+  //       print('$a is negative number.');
+  //   } else if(a==0) {
+  //       print('$a is zero. Neither negative nor positive');
+  //   } else {
+  //       print('$a is positive number.');
+  //   }
+
+
+// stdout.write("Enter the Value :");
+// int a = int.parse(stdin.readLineSync()!);
+// stdout.write("Enter the Value :");
+// int b = int.parse(stdin.readLineSync()!);
+// stdout.write("Enter the Value :");
+// int c = int.parse(stdin.readLineSync()!);
+// if (a > b && a > c) {
+//   print("${a} is Greater Number");
+//   if (b > c) {
+//     print("${b} is Middle Number");
+//     print("${c} is Less Number");
+//   } else if (b < c) {
+//     print("${c} is Middle Number");
+//     print("${b} is Less Number");
+//   }
+// } else if (b > a && b > c) {
+//   print("${b} is Greater Number");
+//   if (c > a) {
+//     print("${c} is Middle Number");
+//     print("${a} is Less Number");
+//   } else if (c < a) {
+//     print("${a} is Middle Number");
+//     print("${c} is Less Number");
+//   }
+// } else if (c > a && c > b) {
+//   print("${c} is Greater NUmber");
+
+//   if (a > b) {
+//     print("${a} is Middle Number");
+//     print("${b} is Less Number");
+//   } else if (a < b) {
+//     print("${b} is Middle Number");
+//     print("${a} is Less Number");
+//   }
+// }
+
+//   stdout.write("User name :");
+//   var a=stdin.readLineSync();
+//   print("Stock:\n \n1) Book Price 100\n2) Beg Price 500 \n3) Pen Price :10\n");
+//   stdout.write("Enter Product name :");
+//   var product=stdin.readLineSync();
+//   stdout.write("Enter Quantity :");
+//   var c=int.parse(stdin.readLineSync()!);
+//   var p1=100;
+//   var p1_qty=8;
+//   var p2=500;
+//   var p2_qty=6;
+//   var p3=10;
+//   var p3_qty=4;
+//   if(product=="Book"){
+//   if(c<p1_qty){
+//     print("You get this item");
+//     var price=c*p1;
+//     print("Price: ${price}");
+// }
+// else{
+//   print("Out of Stock");
+// }
+//   }
+
+// if(product=="Beg"){
+//   if(c<p2_qty){
+//     print("You get this item");
+//     var price=c*p2;
+//     print("Price: ${price}");
+// }
+// else{
+//   print("Out of Stock");
+// }
+// }
+// if(product=="Pen"){
+//   if(c<p3_qty){
+//     print("You get this item");
+//     var price=c*p3;
+//     print("Price: ${price}");
+// }
+// else{
+//   print("Out of Stock");
+// }
+// }
