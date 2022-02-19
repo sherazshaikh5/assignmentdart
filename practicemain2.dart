e087fe20b47e1b317a43da91a90fabb9859b7583
Sheraz Shaikh <98872326+sherazshaikh5@users.noreply.github.com>
2022-02-19 00:53:34 -0800

    Add files via upload

---
+++ practicemain2.dart
@@ -0,0 +1,28 @@
+
+import 'dart:io';
+
+void main(){
+  // stdout.write("Enter the Name :");
+  // String name=stdin.readLineSync()!;
+  // var space=name.indexOf(" ");
+  // var res=name[0].toUpperCase()+name.substring(1,space+1).toLowerCase()+name[space+1].toUpperCase()+name.substring(space+2);
+  // print(res);
+  // print("\n");
+
+  // stdout.write("Enter the Any Character :");
+  // String ac=stdin.readLineSync()!;
+  // var text=ac.compareTo("Text");
+  // print(text);
+  // print("\n");
+
+  // stdout.write("Enter the Any Character :");
+  // String ac1=stdin.readLineSync()!;
+  // print("${ac1.length}");
+  // print("\n");
+
+
+// var incr= --a - --b + ++b + b--;
+//   print(--a);
+//   print(incr);
+  
+}
