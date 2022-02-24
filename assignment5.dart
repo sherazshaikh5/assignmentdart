66b74157157067959b906d381514d8c6a5566f03
Sheraz Shaikh <98872326+sherazshaikh5@users.noreply.github.com>
2022-02-24 08:35:26 -0800

    Add files via upload

---
+++ assignment5.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+
+void main(){
+  // print("Task 01 Initialize List String:\n");
+  // List strlist=["sheraz","anus","asim","naveed","taha"];
+  // print(strlist);
+  // print("\n");
+
+  // print("Task 02 Initialize Mixed List:\n");
+  // List mixlist=[1,2,3,true,false,6,7,8,"anus","sheraz"];
+  // print(mixlist);
+  // print("\n");
+
+  // print("Task 03 Initialize Education List:\n");
+  // List edulist=["SSC","HSC","BCS","BS","BCOM","MS","M. Phil.","PhD"];
+  // print(edulist);
+  // print("\n");
+
+  // print("Task 04 Student Name List:\n");
+  // List stdlist=["sheraz","anus","naveed"];
+  // List stdmarks=[465,345,265];
+  // print("Student Name is ${stdlist[0]} Percentage is ${stdmarks[0]/500*100}");
+  // print("Student Name is ${stdlist[1]} Percentage is ${stdmarks[1]/500*100}");
+  // print("Student Name is ${stdlist[2]} Percentage is ${stdmarks[2]/500*100}");
+  // print("\n");
+
+  // print("Task 05 Sort Ascending Order List:\n");
+  // List stdscore=[243,453,342,564,674,145,786,987];
+  // stdscore.sort();
+  // print(stdscore);
+  // print("\n");
+
+  // print("Task 06 Sort Descending Order List:\n");
+  // List stdscore1=[100,90,80,70,60,50,40,30,20,10];
+  // print(stdscore1.reversed);
+  // print("\n");
+
+  // print("Task 07 Progarm Update Value for Index:\n");
+  // List mixlist1=[1,2,3,true,false,6,7,8,"anus","sheraz"];
+  // stdout.write("Enter Starting Index: ");
+  // int inp1=int.parse(stdin.readLineSync()!);
+  // stdout.write("Enter Ending Index: ");
+  // int inp2=int.parse(stdin.readLineSync()!);
+  // if(inp1>mixlist1.length || inp2>mixlist1.length){
+  //   print("Enter Correct Index Number");
+  // }
+  // else if(inp2>inp1){
+  //   stdout.write("Enter the New Value Add: ");
+  //   String output=stdin.readLineSync()!;
+  //   mixlist1.replaceRange(inp1, inp2, [output]);
+  //   print("Present Index");
+  //   print(mixlist1);
+  // }else{
+  //   print("Not Present Index");
+  // }print("\n");
+
+print("Task 08 Progarm Color List:\n");
+List color=["green","yellow","red","blue","purple","orange","white","black"];
+print(color);
+stdout.write("What you want to add color:");
+String add=stdin.readLineSync()!;
+if(add==color){
+  color.add("red");
+  
+}
+
+// print("Task 09 Progarm Initialize Cities Name List:\n");
+
+// List city=["Karachi","Hyderabad","Sargodha","Islamabad"];
+// print(city);
+// print("\n");
+// print("${city[1]}, ${city[2]}");
+  
+// print("Task 10 Progarm Initialize Cities Name List:\n");
+
+
+
+}
+  
