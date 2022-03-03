e0266bdee4eaf42e03761d068949d5159d2e0fa5
Sheraz Shaikh <98872326+sherazshaikh5@users.noreply.github.com>
2022-03-02 22:32:58 -0800

    Add files via upload

---
+++ loop.dart
@@ -0,0 +1,26 @@
+void  main(){
+  // var i=1;
+  // while (i<=5) {
+  //   print("Hello World");
+  //   i = i+1;
+  // }
+
+  // var num =5;
+  // var factorial =1;
+  // for(var i = num ; i>= 1; i--){
+  //   factorial *=i;
+  // }
+  // print(factorial);
+
+  // for(var temp, i =0, j =1; j<30; temp = i, i = j, j = i + temp){
+  //   print("${j}");
+  // }
+
+//   for(var temp, i = 0, j = 1; j<30; temp = i, i = j, j = i + 1) { 
+//       print('${j}'); 
+//    }
+// for(int as=0;as<=10;++as){
+//   print("hoolo $as");
+// }int ac=0;
+// print("value of ac :${++ac}");
+}
