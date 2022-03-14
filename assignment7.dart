1c11298f51a3e86ca543097540255a68f4dd15ac
Sheraz Shaikh <98872326+sherazshaikh5@users.noreply.github.com>
2022-03-14 01:50:47 -0700

    Add files via upload

--- assignment7.dart
+++ assignment7.dart
@@ -1,41 +1,62 @@
 import 'dart:io';
-
 import 'dart:math';
 
-void main(){
-  // print("Q: 01\n");
+void main() {
+// print("Q: 01\n");
   // stdout.write("enter first number: ");
-  // var a=int.parse(stdin.readLineSync()!);
+  // var fnum=int.parse(stdin.readLineSync()!);
   //  stdout.write("enter second number: ");
-  // var b=int.parse(stdin.readLineSync()!);
-  // pow(a, b);
+  // var snum=int.parse(stdin.readLineSync()!);
+  // pow(fnum, snum);
   // int p=1;
-  // while(b!=0){
-  //   p*=a;
-  //   b--;
-  // }if(b>=0){
+  // while(snum!=0){
+  //   p*=fnum;
+  //   snum--;
+  // }if(snum>=0){
   //   print(p);
-  // }else if(b==0){
-  //   print("result of ${p}");
+  // }else if(snum==0){
+  //   print("result of, ${p}");
   // }print("\n");
+
+//   print("Q: 02\n");
+//   stdout.write("enter any year: ");
+//   var ay = int.parse(stdin.readLineSync()!);
+//   var ly = leapyear(ay);
+//   if (ly == true) {
+//     print("${ay} is a leap year.");
+//   } else {
+//     print("${ay} is not a leap year.");
+//   }
+// }
+// bool leapyear(data) {
+//   if (data % 4 == 0) {
+//     return true;
+//   }
+//   else {
+//     print("please enter correct year.");
+//     return false;
+//   }
+
+//   print("Q: 03\n");
+//   var a=int.parse(stdin.readLineSync()!);
+//   var b=int.parse(stdin.readLineSync()!);
+//   var c=int.parse(stdin.readLineSync()!);
+//   var res=Calculates(a,b,c);
+// }
+
+//   void Calculates(a,b,c){
+//     var s=(a+b+c)/2;
+//     print("divide: ${s}");
+//     calculatearea(a,b,c,s);
+//   }
+//   void calculatearea(a,b,c,s){
+//     num area=s*(s-a)*(s-b)*(s-c);
+//     print("Result: ${area}");
+//   }
+   print("Q: 04\n");
+   
+   
   
-print("Q: 02\n");
-List lyear=[2012,2016,2020];
-stdout.write("enter any year: ");
-var y=int.parse(stdin.readLineSync()!);
-leapyear(){
-  if(y%4==lyear.length){
-    print(" Yes this Leap year.");
-    years(y,lyear);
-  }
- 
- years(y,lyear){
-    if(y%4==lyear.length){
-    print(" Yes this Leap year.");
-
-  }
-
- }
-}
+  
 
 }
