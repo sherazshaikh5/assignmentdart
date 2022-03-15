10a582aad2366af234c75401215197c9b0cc6077
Sheraz Shaikh <98872326+sherazshaikh5@users.noreply.github.com>
2022-03-15 13:27:20 +0500

    Add files via upload

--- functions.dart
+++ functions.dart
@@ -1,31 +1,52 @@
 import 'dart:io';
 
 void main() {
-   print("Q: 04\n");
-   stdout.write("enter sub1 marks: ");
-   var sub1=int.parse(stdin.readLineSync()!);
-   stdout.write("enter sub2 marks: ");
-   var sub2=int.parse(stdin.readLineSync()!);
-   stdout.write("enter sub3 marks: ");
-   var sub3=int.parse(stdin.readLineSync()!);
-   var c=Calculatesum(sub1,sub2,sub3);  
-}
-void Calculatesum(sub1,sub2,sub3){
-  var c=(sub1+sub2+sub3);
-  print(c);
-  Calculateaverage(c);
-}
-void Calculateaverage(c){
-  if(c==3){
-    print("Average is, ${c/3}");
-    Percentage(c);
+  // stdout.write("Enter Id: ");
+  // var id=int.parse(stdin.readLineSync()!);
+  // stdout.write("Enter Username: ");
+  // var unam=stdin.readLineSync()!;
+  // stdout.write("Enter Score: ");
+  // var score=int.parse(stdin.readLineSync()!);
+
+  var a = File("new.txt");
+  a.writeAsStringSync("Id\tName\t\tScore\n");
+  a.writeAsStringSync(a.readAsStringSync() + "1\tAqib\t\t85\n");
+  a.writeAsStringSync(a.readAsStringSync() + "2\tAmin\t\t78\n");
+  a.writeAsStringSync(a.readAsStringSync() + "3\tZia\t\t65\n");
+  a.writeAsStringSync(a.readAsStringSync() + "4\tNoman\t\t56\n");
+  a.writeAsStringSync(a.readAsStringSync() + "5\tSajjad\t\t45\n");
+  var getdata=a.readAsLinesSync();
+  for(var a=1; a<getdata.length; a++){
+    var newdata=getdata[a].toString().split("\t");
+    print("User Id: ${newdata[0]}\t User Name: ${newdata[1]}\t User Score: ${newdata[3]}");
   }
-}
-void Percentage(c){
-  var tmarks=850;
-  print("Percentage is ${c/tmarks*100}");
-  return;
-}
+  
+
+//    print("Q: 04\n");
+//    stdout.write("enter sub1 marks: ");
+//    var sub1=int.parse(stdin.readLineSync()!);
+//    stdout.write("enter sub2 marks: ");
+//    var sub2=int.parse(stdin.readLineSync()!);
+//    stdout.write("enter sub3 marks: ");
+//    var sub3=int.parse(stdin.readLineSync()!);
+//    var c=Calculatesum(sub1,sub2,sub3);
+// }
+// void Calculatesum(sub1,sub2,sub3){
+//   var c=(sub1+sub2+sub3);
+//   print(c);
+//   Calculateaverage(c);
+// }
+// void Calculateaverage(c){
+//   if(c==3){
+//     print("Average is, ${c/3}");
+//     Percentage(c);
+//   }
+// }
+// void Percentage(c){
+//   var tmarks=850;
+//   print("Percentage is ${c/tmarks*100}");
+//   return;
+// }
 //   stdout.write("enter first number: ");
 //   var a = int.parse(stdin.readLineSync()!);
 //   stdout.write("enter second number: ");
@@ -43,6 +64,4 @@ void Percentage(c){
 //     print("result is sum, ${a + b + c}");
 //     print("result is divide, ${b / c}");
 //     return;
-// }
-
-
+}
