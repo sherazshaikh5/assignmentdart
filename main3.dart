9f205cc3e23be02a5d7f94f3e1348a3f48788439
Sheraz Shaikh <98872326+sherazshaikh5@users.noreply.github.com>
2022-03-17 23:18:04 -0700

    Add files via upload

---
+++ main3.dart
@@ -0,0 +1,80 @@
+import 'dart:io';
+
+void main(){
+//   Car c=new Car(wheel:4,name:"honda");
+//   c.printdata();
+ 
+// class Car{
+//   Car(int? wheel, String? name){
+//   this.wheel=wheel;
+//   this.name=namedata;
+// }
+
+//   int? wheel;
+//   String? name;
+//   void printdata(){
+//     print(wheel);
+//     print(name);
+//   }
+
+// }
+// var object_name = new class_name([ arguments ])
+// Example:
+// Take a look at the following example to understand how to access attributes and functions in Dart.
+// Car c=new Car();
+// c.disp();
+// }
+// class Car {
+// String name="Honda Civic";
+// String engine= "E1001";
+// int wheel=4;
+
+// void disp(){
+//   print("Car name: ${name}");
+//   print("Engine: ${engine}");
+//   print("Wheel: ${wheel}");
+  
+// }
+// }
+// Example:
+// The following example shows how to use constructors in Dart −
+//   Car c=new Car('Honda Civic','E1001',4);
+// }
+// class Car{
+//   Car(String name, String engine, int wheel){
+//   print(name);
+//   print(engine);
+//   print(wheel);
+// }
+
+// Syntax : Defining the constructor
+// Class_name.constructor_name(param_list)
+// Example:
+// The following example shows how you can use named constructors in Dart
+// Car c1=new Car.namedConst('Honda Civic','E1001',4);
+// // Car c2=new Car();
+
+// }
+// class Car{
+//   // Car(){
+//   //   print("Non-parameterized contructor invoked");
+//   // }
+//   Car.namedConst(String name,String engine,int wheel){
+//     print("Car name: ${name}");
+//     print("Enging is: ${engine}");
+//     print("Number of wheel: ${wheel}");
+//   }
+
+// Example:
+// The following example explains how to use the this keyword in Dart
+// Car c1=new Car('Honda Civic','E1001',4); 
+// }
+// class Car{
+//   String? engine;
+//   Car(String name,String engine, int wheel){
+//     this.engine=engine;
+//     print("Car Name: ${name}");
+//     print("Engine is: ${engine}");
+//     print("Number of Wheel: ${wheel}");
+//   }
+}
